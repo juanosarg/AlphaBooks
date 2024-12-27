f474b1aee67d3e458826430d9826173959755948
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-12-27 19:55:30 +0100

    v0.1

---
+++ 1.5/Source/AlphaBooks/Harmony/Toils_General_Open.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+using static UnityEngine.GridBrushBase;
+
+namespace AlphaBooks
+{
+
+    [HarmonyPatch(typeof(Toils_General))]
+    [HarmonyPatch("Open")]
+    public static class AlphaBooks_Toils_General_Open_Patch
+    {
+
+
+        [HarmonyPostfix]
+        public static void CheckForHermetic(ref Toil __result)
+        {
+            __result.AddFinishAction(delegate
+            {
+               // Log.Message("Opened");
+
+            });
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
