9de3ac43aad2b516ba7d311b78e79aed47844e16
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-12-30 19:53:59 +0100

    v0.1

---
+++ 1.5/Source/AlphaBooks/Books/Book_Plant.cs
@@ -0,0 +1,24 @@
+﻿
+using RimWorld;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using Verse.Grammar;
+namespace AlphaBooks
+{
+    public class Book_Plant : Book_NotAutoReadable
+    {
+        public ThingDef plant;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Defs.Look(ref plant, "plant");
+
+        }
+
+
+    }
+}
