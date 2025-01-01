b1cdfc7a0589c79ffc34b83bbb37c9dfd5ac517a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-01-01 09:35:39 +0100

    v0.2

---
+++ 1.5/Source/AlphaBooks/Harmony/Dialog_ManageReadingPolicies_PolicyGlobalFilter.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Verse.AI;
+using AlphaBooks;
+
+
+
+namespace AlphaBooks
+{
+
+
+    [HarmonyPatch(typeof(Dialog_ManageReadingPolicies))]
+    [HarmonyPatch("PolicyGlobalFilter", MethodType.Getter)]
+    public static class AlphaBooks_Dialog_ManageReadingPolicies_PolicyGlobalFilter_Patch
+    {
+        private static ThingFilter policyGlobalFilterInternal;
+
+        [HarmonyPostfix]
+        static void ModifyPolicyFilter(ref ThingFilter ___policyGlobalFilter, ref ThingFilter __result)
+        {
+            if (policyGlobalFilterInternal == null)
+            {
+                policyGlobalFilterInternal = new ThingFilter();
+                foreach (ThingDef item in ___policyGlobalFilter.allowedDefs.Where((ThingDef x) => !x.defName.Contains("ABooks_")))
+                {
+                    policyGlobalFilterInternal.SetAllow(item, allow: true);
+                   
+                }
+                ___policyGlobalFilter = policyGlobalFilterInternal;
+            }
+            __result =  policyGlobalFilterInternal;
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
